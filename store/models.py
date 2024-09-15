fa341d56a68c5fd25553acdf0b5f9667240548f4
AmirHossein Esmaeili <69587936+AmrhsnEs@users.noreply.github.com>
2024-09-15 17:08:35 +0330

    update model

--- store/models.py
+++ store/models.py
@@ -18,7 +18,8 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     desctiption = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=0)
+    slug = models.SlugField(default="-")
+    unit_price = models.DecimalField(max_digits=6, decimal_places=0)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
