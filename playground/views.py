aa79c55d8e3dab45877eaf983f0cf867a95b1b6b
AmirHossein Esmaeili <69587936+AmrhsnEs@users.noreply.github.com>
2024-09-18 17:27:09 +0330

    Update Setting

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import OrderItem, Product
 
 
 def say_hello(request):
-    query_set = Product.objects.get(pk=1)
 
-    return render(request, "hello.html", {"name": "Mosh"})
+    products = Product.objects.filter(
+        id__in=OrderItem.objects.values("product__id").distinct()
+    ).order_by("title")
+    context = {
+        "products": list(products),
+    }
+    return render(request, "hello.html", context)
