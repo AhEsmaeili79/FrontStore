06633a23c269358efef98a940234804c9be146ac
AmirHossein Esmaeili <69587936+AmrhsnEs@users.noreply.github.com>
2024-09-20 00:08:12 +0330

    Update views

--- store/views.py
+++ store/views.py
@@ -1,8 +1,10 @@
 import http
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
-# Create your views here.
+@api_view()
 def product_list(request):
-    return HttpResponse("Ok")
+    return Response("Ok")
