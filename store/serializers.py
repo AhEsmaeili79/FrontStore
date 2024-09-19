0de20d5e92a11e3cff62ce01df02c08391404f68
AmirHossein Esmaeili <69587936+AmrhsnEs@users.noreply.github.com>
2024-09-20 01:15:49 +0330

    update serializers.py

--- store/serializers.py
+++ store/serializers.py
@@ -3,34 +3,48 @@ from rest_framework import serializers
 from store.models import Product, Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ["id", "title"]
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(
-        max_digits=6, decimal_places=0, source="unit_price"
-    )
+# for custome serializer we use :
+# class ProductSerializer(serializers.Serializer):
+
+
+# for model Serializers we use :
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "title", "unit_price", "price_with_tax", "collection"]
+
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
-
-    # serializing relationships
-
-    # first Way String
-    # collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all())
-
-    # Second Way String
     collection = serializers.StringRelatedField()
 
-    # Third Way Nested Objects
-    # collection = CollectionSerializer()
-
-    # fourth Way Hyperlink
-    # collection = serializers.HyperlinkedRelatedField(
-    #     queryset=Collection.objects.all(), view_name="collection-detail"
+    # Custome Serializers
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(
+    #     max_digits=6, decimal_places=0, source="unit_price"
     # )
+    # price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+
+    # # serializing relationships
+
+    # # first Way String
+    # # collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all())
+
+    # # Second Way String
+    # collection = serializers.StringRelatedField()
+
+    # # Third Way Nested Objects
+    # # collection = CollectionSerializer()
+
+    # # fourth Way Hyperlink
+    # # collection = serializers.HyperlinkedRelatedField(
+    # #     queryset=Collection.objects.all(), view_name="collection-detail"
+    # # )
 
     def calculate_tax(self, product: Product):
-        return product.unit_price * Decimal(1.1)
+        return round(product.unit_price * Decimal(1.1), 2)
