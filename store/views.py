48d885045c0525fff567bddf75286bc0453dfe14
AmirHossein Esmaeili <69587936+AmrhsnEs@users.noreply.github.com>
2024-09-21 18:15:12 +0330

    add pagination.py

--- store/views.py
+++ store/views.py
@@ -1,13 +1,15 @@
+from django.db.models import Count
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
-from rest_framework.filters import SearchFilter
-from store.filters import ProductFilter
-from .models import OrderItem, Product, Collection, Review
-from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.exceptions import ValidationError
 from rest_framework import status
-from django.db.models import Count
 from rest_framework.viewsets import ModelViewSet
-from django_filters.rest_framework import DjangoFilterBackend
+from .models import OrderItem, Product, Collection, Review
+from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
+from .filters import ProductFilter
+from .pagination import DefaultPagination
 
 
 class ProductViewSet(ModelViewSet):
@@ -15,9 +17,12 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
 
     #  we could use this for filter instead of
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
-    search_fields = ["title", "description", "collection__title"]
+    search_fields = ["title", "description"]
+    ordering_fields = ["unit_price", "last_update"]
+    pagination_class = DefaultPagination
+
     #  using this for filter
     # def get_queryset(self):
     #     queryset = Product.objects.all()
